b726e463cd6fc7cc39f3827624b507260409536e
Rattanapoom <614259058@webmail.npru.ac.th>
2020-11-10 21:05:57 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+//const express = require('express');
+//const app = express();
+ 
+//app.get('/',(req,res) => {//เเบบใหม่ name => {  }
+    //res.send("<h1>My Portfolio</h1>");//res.send ส่งข้อมูลไปหน้าเว็ป
+//});
+
+//app.listen(//Port3000 
+  //  3000,
+    //()=>{
+     //   console.log("listening to port 3000");
+    //}
+//);
+const express = require('express');
+const router =require('./routes/index');
+const app = express();
+const hbs = require('express-handlebars');
+const path = require('path');
+
+app.engine('hbs',hbs({extname:'hbs'}));
+app.set('view engine', 'hbs');
+app.use('/profile',router);
+app.use(express.static(path.join(__dirname,'public')));
+
+
+
+app.listen(//Port3000 
+    3000,
+    ()=>{
+        console.log("listening to port 3000");
+    }
+);
+
